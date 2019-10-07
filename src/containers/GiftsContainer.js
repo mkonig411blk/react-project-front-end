2aa6cf7a0e3790c62c235e7b7db87ce49180ee22
mkonig411blk <margot.konig@blackrock.com>
2019-10-07 14:49:38 -0500

    added structure to app.js & giftscontainer.js

---
+++ src/containers/GiftsContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+// import RestaurantInput from '../components/restaurants/RestaurantInput'
+// import Gifts from '../components/gifts/Gifts'
+// import { connect } from 'react-redux'
+
+class RestaurantsContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        TEST
+      </div>
+    )
+  }
+}
+
+// const mapStateToProps = ({ restaurants }) => ({ restaurants })
+//
+// const mapDispatchToProps = dispatch => ({
+//   addRestaurant: name => dispatch({ type: "ADD_RESTAURANT", name }),
+//   deleteRestaurant: id => dispatch({type: "DELETE_RESTAURANT", id})
+// })
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer)
+
+export default RestaurantsContainer
